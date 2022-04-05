b74deda2a8dd71a6385cc6f65f16481cda820cef
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-04-05 20:44:42 +0530

    rev array 14, 15

--- Arrays/Day-3/15-Next Permutation/index.cpp
+++ Arrays/Day-3/15-Next Permutation/index.cpp
@@ -1,35 +1,44 @@
 // T.C: O(n) and S.C: O(1)
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int> &nums)
+    {
         int n = nums.size(), k, l;
-    	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
+        for (k = n - 2; k >= 0; k--)
+        {
+            if (nums[k] < nums[k + 1])
+            {
                 break;
             }
         }
-    	if (k < 0) {
-    	    reverse(nums.begin(), nums.end());
-    	} else {
-    	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
+        if (k < 0)
+        {
+            reverse(nums.begin(), nums.end());
+        }
+        else
+        {
+            for (l = n - 1; l > k; l--)
+            {
+                if (nums[l] > nums[k])
+                {
                     break;
                 }
-            } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
+            }
+            swap(nums[k], nums[l]);
+            reverse(nums.begin() + k + 1, nums.end());
         }
     }
 };
 int main()
 {
-    vector<int> nums{3,2,1};
+    vector<int> nums{3, 2, 1};
     Solution s;
     s.nextPermutation(nums);
-    for(int i = 0; i < nums.size(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
         cout << nums[i] << " ";
     }
